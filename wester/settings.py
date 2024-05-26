2d2ab8cb66304ecb2e37ee186d92717e3aaebde2
Jasurbek <jasurkamilov7279@gmail.com>
2024-05-26 23:15:26 +0500

    .

--- wester/settings.py
+++ wester/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ih98g8*zv09heuu_fg*t+fbyj9&=s2jvha_#+gpkpk41d*=67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # if DEBUG:
 #     from wester.locale_settings import *
@@ -219,20 +219,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),  
 ]
 
 
-STATIC_URL = '/wester_academy/static/'
-STATIC_ROOT = '/home/westera1/wester_academy/static/'
-MEDIA_URL = '/wester/media/'
-MEDIA_ROOT = '/home/westera1/wester_academy/media/'
+# STATIC_URL = '/wester_academy/static/'
+# STATIC_ROOT = '/home/westera1/wester_academy/static/'
+# MEDIA_URL = '/wester/media/'
+# MEDIA_ROOT = '/home/westera1/wester_academy/media/'
 
 
 # Default primary key field type
