0a40c29103574d471c6be77485371909fbde9dad
Jasurbek <jasurkamilov7279@gmail.com>
2024-05-26 23:22:18 +0500

    .

--- wester/settings.py
+++ wester/settings.py
@@ -219,20 +219,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),  
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles'),  
+# ]
 
 
-# STATIC_URL = '/wester_academy/static/'
-# STATIC_ROOT = '/home/westera1/wester_academy/static/'
-# MEDIA_URL = '/wester/media/'
-# MEDIA_ROOT = '/home/westera1/wester_academy/media/'
+STATIC_URL = 'wester_academy/static/'
+STATIC_ROOT = 'home/westera1/wester_academy/static/'
+MEDIA_URL = 'wester_academy/media/'
+MEDIA_ROOT = 'home/westera1/wester_academy/media/'
 
 
 # Default primary key field type
